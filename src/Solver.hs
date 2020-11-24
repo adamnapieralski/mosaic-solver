1d0511ac15381f0e7c32626c431a8df0ebbcc46f
adamnapieralski <adam.napieralski@gmail.com>
2020-11-24 21:10:22 +0100

    add neighbours and fill related funcs to Board

--- src/Solver.hs
+++ src/Solver.hs
@@ -1,5 +1,10 @@
 module Solver where
 import Board
+import Neighbour
 
 solve :: Board -> Board
-solve inPuzzle = makeEmptyWithSize (getH(inPuzzle)) (getW(inPuzzle))
+solve inPuzzle = fillCell (inPuzzle) (getNbY inPuzzle N 1) (getNbX inPuzzle N 1)
+    -- (makeEmptyWithSize (case (getNbY inPuzzle NW 0) of
+    --                                     Just y -> y
+    --                                     Nothing -> 50) (getW(inPuzzle)))
+
