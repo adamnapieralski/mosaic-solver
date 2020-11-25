56c1a09ce26c15e0f72ca1ab4fcff089cdd8cbee
adamnapieralski <adam.napieralski@gmail.com>
2020-11-25 16:13:15 +0100

    ft neighbs: added nbs filling related functions

--- src/Board.hs
+++ src/Board.hs
@@ -3,6 +3,9 @@ module Board where
 import Neighbour
 import Utils
 
+-- used this import, dont know if it isnt against task, but cant think of other way
+import Data.Char
+
 data Board = Board [String] deriving (Show)
 
 row :: Board -> Int -> String
@@ -28,12 +31,21 @@ getNbY board nb y   | elem nb [NW, N, NE] = (if inRange (y-1) 0 (getH board) the
 
 
 getNbX :: Board -> Neighbour -> Int -> Maybe Int
-getNbX board nb x   | elem nb [NE, E, SE] = (if inRange (x-1) 0 (getW board) then Just (x-1) else Nothing)
+getNbX board nb x   | elem nb [NE, E, SE] = (if inRange (x+1) 0 (getW board) then Just (x+1) else Nothing)
                     | elem nb [N, C, S] = Just x
-                    | elem nb [NW, W, SW] = (if inRange (x+1) 0 (getW board) then Just (x+1) else Nothing)
+                    | elem nb [NW, W, SW] = (if inRange (x-1) 0 (getW board) then Just (x-1) else Nothing)
 
 replaceCell :: Board -> Int -> Int -> Char -> Board
 replaceCell (Board board) y x newCh = Board (replaceNth y (replaceNth x newCh (row (Board board) y)) board)
+
+isNeighbourValid :: Board -> Neighbour -> Int -> Int -> Bool
+isNeighbourValid board n y x =
+    case getNbY board n y of
+        Just y -> case getNbX board n x of
+            Just x -> True
+            Nothing -> False
+        Nothing -> False
+
 
 fillCell :: Board -> Maybe Int -> Maybe Int -> Board
 fillCell markBoard y x =
@@ -42,3 +54,43 @@ fillCell markBoard y x =
             Just x -> replaceCell markBoard y x 'X'
             Nothing -> markBoard
         Nothing -> markBoard
+
+fillNeighbours :: Board -> [Neighbour] -> Int -> Int -> Board
+fillNeighbours markBoard [] _ _ = markBoard
+fillNeighbours markBoard (n:nbs) y x =
+    fillNeighbours (fillCell markBoard (getNbY markBoard n y) (getNbX markBoard n x))
+        nbs y x
+
+isCellFilled :: Board -> Maybe Int -> Maybe Int -> Maybe Bool
+isCellFilled _ Nothing _ = Nothing
+isCellFilled _ _ Nothing = Nothing
+isCellFilled markBoard y x = 
+    case y of
+        Just y -> case x of
+            Just x -> Just (element markBoard y x == 'X')
+            Nothing -> Nothing
+        Nothing -> Nothing
+
+checkNeighboursFill :: Board -> [Neighbour] -> Int -> Int -> [Maybe Bool]
+checkNeighboursFill markBoard nbs y x = go markBoard nbs [] y x where
+    go _ [] fills _ _ = fills
+    go markBoard (n:nbs) fills y x = go markBoard nbs (fills ++ [(isCellFilled markBoard (getNbY markBoard n y) (getNbX markBoard n x))]) y x
+
+countBlankNeighbours :: Board -> Int -> Int -> Int
+countBlankNeighbours markBoard y x = go markBoard (checkNeighboursFill markBoard [N, NE, E, SE, S, SW, W, NW, C] y x) y x 0 where
+    go _ [] _ _ counter = counter
+    go markBoard (b:bs) y x counter = go markBoard bs y x (counter + blankAdd) where
+        blankAdd    | b == (Just False) = 1
+                    | otherwise = 0
+
+getValidNeighbours :: Board -> Int -> Int -> [Neighbour]
+getValidNeighbours board y x = go board [C] [N, NE, E, SE, S, SW, W, SW] y x where
+    go _ validNeighbours [] _ _ = validNeighbours
+    go board validNeighbours (n:nbs) y x    | isNeighbourValid board n y x = go board (n:validNeighbours) nbs y x
+                                            | otherwise = go board validNeighbours nbs y x
+
+
+fillFullRemainingOfCell :: Board -> Board -> Int -> Int -> Board
+fillFullRemainingOfCell numBoard markBoard y x
+    | (countBlankNeighbours markBoard y x) == digitToInt (element numBoard y x) = fillNeighbours markBoard (getValidNeighbours markBoard y x) y x
+    | otherwise = markBoard
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 import Solver
 import IOHandler
+import Board
+import Neighbour
 
 main = do
     board <- readBoard "data/input1.txt"
-    let resBoard = solve(board)
-    print resBoard
+    -- let resBoard = solve(board)
+    let resBoard = makeEmptyWithSize (getH board) (getW board)
+    print(checkNeighboursFill resBoard [N, S, W] 0 2)
+    print(getValidNeighbours resBoard 0 0)
+    let filledTL = fillFullRemainingOfCell board resBoard 0 0
+    print (filledTL)
+
+    print board
